68b665617282c7d8702aa71089c2801eee5b02d6
mackliu <macklun@gmail.com>
2026-05-25 11:14:03 +0800

    在subject_students.php 中的sql語法中加入詳細語法說明

--- school/include/subject_students.php
+++ school/include/subject_students.php
@@ -140,8 +140,33 @@ $dept_name=$pdo->query("SELECT `name` FROM `dept` WHERE `id`='{$_GET['dept']}'")
 //從class_student 中找到班級學生的學號
 
 //$sql="select * from `class_student` where `class_code`='{$_GET['code']}'";
+// ===== SQL 查詢語句：獲取指定科別的所有學生詳細資訊 =====
+// SELECT 指令：查詢多個資料表中的指定欄位
+// 選取欄位說明：
+//   - students.school_num：學號（來自學生資料表）
+//   - students.name：姓名（來自學生資料表）
+//   - classes.name as 'class_name'：班級名稱（來自班級資料表，別名為 class_name）
+//   - dept.name as 'dept_name'：科別名稱（來自科別資料表，別名為 dept_name）
+//   - addr：地址（來自學生資料表）
+//   - uni_id：身分證號（來自學生資料表）
+//   - graduate_school.name as 'graduate_school'：畢業國中名稱（來自畢業國中資料表，別名為 graduate_school）
+//   - birthday：出生日期（來自學生資料表）
+// 
+// FROM 子句：關聯四個資料表
+//   - class_student：班級學生對應表
+//   - students：學生資料表
+//   - dept：科別資料表
+//   - graduate_school：畢業國中資料表
+//   - classes：班級資料表
+//
+// WHERE 子句：設定資料表間的關聯條件及篩選條件
+//   - class_student.school_num = students.school_num：班級學生表與學生表關聯（學號相同）
+//   - dept.id = students.dept：科別表與學生表關聯（科別ID相同）
+//   - graduate_school.id = students.graduate_at：畢業國中表與學生表關聯（畢業國中ID相同）
+//   - classes.code = class_student.class_code：班級表與班級學生表關聯（班級代碼相同）
+//   - students.dept = '{$_GET['dept']}'：篩選條件，只查詢該科別的學生
 $sql="select 
-             `students`.`school_num`,
+             `students`.`school_num`, 
              `students`.`name`,
              `classes`.`name` as 'class_name',
              `dept`.`name` as 'dept_name',
