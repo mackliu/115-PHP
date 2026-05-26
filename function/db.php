303380acd29874b146cdf6c8783c221031327cfd
mackliu <macklun@gmail.com>
2026-05-26 11:19:20 +0800

    完成find()函式,並增加可能的錯誤資料判斷

--- function/db.php
+++ function/db.php
@@ -9,12 +9,36 @@ function all($table){
     return $rows; //整個$table 的資料
 }
 
+function find($table,$id){
+    //連線資料庫
+    $dsn="mysql:host=localhost;dbname=school;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
+
+    if(!is_numeric($id)){
+        echo "ID 必須為數字";
+        return false;
+    }else if($id<1){
+        echo "ID 必須大於等於 1";
+        return false;
+    }else if(!$pdo->query("SELECT count(*) FROM $table WHERE `id`='$id'")->fetchColumn()){
+        echo "找不到指定的資料";
+        return false;
+    }
+
+    $row=$pdo->query("SELECT * FROM $table WHERE `id`='$id'")->fetch(PDO::FETCH_ASSOC);
+
+ return $row;
+}
+
 /* echo "<pre>";
  print_r(all('status'));
  echo "</pre>"; */
 
 $rows=all('status');
-
+$row=find('status',1);
+echo "<pre>";
+ print_r($row);
+ echo "</pre>";
 ?>
 
 
