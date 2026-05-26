80ff9d24d35062f84a0945ef6b2e365d7c2d35ac
mackliu <macklun@gmail.com>
2026-05-26 10:53:16 +0800

    使用自訂函式完成all()-取出指定資料表的所有資料

--- function/index.php
+++ function/index.php
@@ -34,8 +34,12 @@ name("劉","勤永");
 ?>
 <h2>畫星星函式</h2>
 <?php 
-triangle_stars(10);
-square_stars(20);
+/* triangle_stars(5);
+square_stars(5);
+diamond_stars(22); */
+stars('diamond',10);
+stars('triangle',10);
+stars('square',10);
 ?>
 </body>
 </html>
@@ -63,8 +67,24 @@ function sum($title,...$nums){
    echo "<br>";
 }
 
+function stars($shape="triangle",$size=5){
+    switch($shape){
+        case "triangle":
+            triangle_stars($size);
+            break;
+        case "square":
+            square_stars($size);
+            break;
+        case "diamond":
+            diamond_stars($size);
+            break;
+        default:
+            echo "不支援的圖形";
+    }
+}
+
+
 function triangle_stars($size=5){
-    
 for($i=0;$i<$size;$i++){
     for($j=0;$j<$size-1-$i;$j++){
         echo "&nbsp;";
@@ -89,7 +109,35 @@ function square_stars($size=5){
             }
         }
         echo "<br>";
+    }
+}
 
+function diamond_stars($size=5){
+    if(!is_numeric($size)){
+        echo "n 必須為數字<br>";
+        $size=5;
+    }
+    if($size<3){
+        echo "n 必須大於等於 3<br>";
+        $size=5;
+    }
+    if($size%2==0){
+        $size=$size+1;
+    }
+    for($i=0;$i<$size;$i++){
+        if($i<=floor($size/2)){
+            $t=$i;
+        }else{
+            $t=$size-1-$i;
+        }
+
+        for($j=0;$j<floor($size/2)-$t;$j++){
+            echo "&nbsp;";
+        }
+        for($k=0;$k<2*$t+1;$k++){
+            echo "*";
+        }
+        echo "<br>";
     }
 }
 ?>
